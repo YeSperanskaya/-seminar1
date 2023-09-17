8020f0dbca226688be54406c3aa2d49c69d77ef3
Evgeniya <speranskaya1991@mail.ru>
2023-09-17 13:45:36 +0500

    added task01

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA / numberB == numberB)
+{
+    Console.Write(numberA + " of " + numberB + " is Square root)");
+}
+else
+{
+    Console.Write(numberA + " of " + numberB + "is Square root)");
+}
