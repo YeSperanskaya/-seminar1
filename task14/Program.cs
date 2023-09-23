3ecad566970a41ecc0c2fb61b5b3aa8548dfdffc
Evgeniya <speranskaya1991@mail.ru>
2023-09-23 13:57:58 +0500

    added task14

---
+++ task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+Console.WriteLine("Please enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool Division7And23 (int num)
+{
+    int firstNumber = 7;
+    int secondNumber = 23;
+    int num1 = num % firstNumber;
+    int num2 = num % secondNumber;
+    if (num1 & num2 == 0) return yes;
+    esle return no;
+} 
+bool division = Division7And23(45);
+Console.WriteLine($"result {number} -> division");
