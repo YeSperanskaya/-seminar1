ad6db97d3b10c7ad669ea0fda9ad23d2452c89f9
Evgeniya <speranskaya1991@mail.ru>
2023-09-24 12:54:57 +0500

    added task18

---
+++ task18/Program.cs
@@ -0,0 +1,20 @@
+﻿string QuarterToCoords(int num)
+{ 
+    if(num == 1) return "x > 0, y > 0";
+    if(num == 2) return "x < 0, y > 0";
+    if(num == 3) return "x < 0, y < 0";
+    if(num == 4) return "x > 0, y < 0";
+    return null;
+}
+
+
+Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string quarter = QuarterToCoords(number);
+
+
+string result = quarter != null ? $"Указанная четверть соответствуют координатам {quarter}" : "Введены  некорректные координаты";
+
+Console.WriteLine(result);
+
