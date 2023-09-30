d023a51b643880e83be9f911be97d20f467ceb76
Evgeniya <speranskaya1991@mail.ru>
2023-09-30 13:38:56 +0500

    решили до 28 задачи

---
+++ task24/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int SumNumbers (int num)
+{
+    
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum = sum + i;
+        }
+        
+    }
+    return sum;
+}
+
+
+
+
+Console.WriteLine("введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if  (number < 1)
+{ 
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} -> {result}");
---
+++ task28/Program.cs
@@ -0,0 +1,30 @@
+﻿int Factorial (int num)
+{
+    
+    int result = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            result = result * i;
+        }
+        
+    }
+    return result;
+}
+
+
+
+
+Console.WriteLine("введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if  (number < 1)
+{ 
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+int resultFactorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {resultFactorial}");
