3a6e2913dae10e347eafc6bf8a588869b4d2d499
Evgeniya <speranskaya1991@mail.ru>
2023-10-01 13:14:46 +0500

    решила 33 задачу

---
+++ task033/Program.cs
@@ -0,0 +1,103 @@
+﻿// // Задача 33: Задайте массив. Напишите программу, которая
+// // определяет, присутствует ли заданное число в массиве.
+// // 4; массив [6, 7, 19, 345, 3] -> нет
+// // 3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     //Console.WriteLine("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+//         else Console.Write($"{arr[i]}");
+//     }
+//     //Console.WriteLine("]");
+// }
+
+// bool FindNumber(int[] arr, int num)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if(arr[i] == num) return true;
+        
+//     }
+//     return false;
+
+// }
+
+// int[] array = CreateArrayRndInt(12, -9, 9);
+// Console.Write("[");
+// PrintArray(array);
+// Console.WriteLine("]");
+
+// Console.WriteLine("Введите искомое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool findNumber = FindNumber(array, number);
+// Console.Write($" Число {")
+// Console.Write((findNumber == true) ? $" -> да" : $" -> нет");
+
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max +1);
+    }
+    return arr;
+
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+    // Console.WriteLine("]"); если добавим в метод, то скобки будут выводится всегда. Если скобки не критичны, то стоит выводить за метод.
+}
+
+bool FindNumber (int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ( arr[i] == num)
+        {
+            return true;
+        }
+
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+// PrintArray(array);
+Console.Write(" [");
+PrintArray(array);
+ Console.WriteLine("]");
+
+ bool findNumber = FindNumber(array, number);
+ Console.WriteLine(findNumber ? "Число найдено ": "Число не найдено");
